1865639cbab9b6d11479de574c3de936dc25e2b3
laksh1256 <lkkhandelwal11nov.com>
2020-06-02 13:33:03 +0530

    updated

--- mod-4sol/app.js
+++ mod-4sol/app.js
@@ -1,5 +1,5 @@
-var arr=["laksh","jatin","jai","yash","sddgcvd","jwjkdbw"];
-for(var i=0;i<6;i++)
+var arr=["Yaakov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
+for(var i=0;i<arr.length;i++)
 { if(arr[i].charAt(0)=='j'|| arr[i].charAt(0)=='J')
 byeSpeaker.speak(arr[i]);
 else helloSpeaker.speak(arr[i]);
